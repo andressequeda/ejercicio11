0c7d0527ea0ba6ecbb4210ed9f2561b1b5bb16fa
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 22:12:53 -0500

    Add files via upload

---
+++ ejercicio11.py
@@ -0,0 +1,2 @@
+altura  =  float ( input ( "Ingrese la altura a la que será lanzado el objeto (en metros):" ))
+print ( "El tiempo de caída del objeto es de" , math . sqrt (( 2  *  altura ) /  10 ), "segundos" )
